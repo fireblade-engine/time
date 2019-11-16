dba47aa77dc8a4c1cfd60af71d240eb4c004c1c3
Christian Treffs <ctreffs@gmail.com>
2019-11-16 22:04:44 +0100

    Add Timer

---
+++ Sources/FirebladeTime/Timer.swift
@@ -0,0 +1,29 @@
+//
+//  Timer.swift
+//
+//
+//  Created by Christian Treffs on 16.11.19.
+//
+
+open class Timer {
+    public var isStopped: Bool = false
+    @usableFromInline let elapsed: (Nanoseconds) -> Void
+    public let start: Nanoseconds
+
+    public init(_ elapsed: @escaping (Nanoseconds) -> Void) {
+        start = Time.now
+        self.elapsed = elapsed
+    }
+
+    deinit {
+        if !isStopped {
+            stop()
+        }
+    }
+
+    @inlinable
+    public func stop() {
+        elapsed(Time.elapsed(start: start, end: Time.now))
+        isStopped = true
+    }
+}
