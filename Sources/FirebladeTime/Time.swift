eaafb1bda2ccd5ec99ae2df93d30dd098aa964f3
Christian Treffs <ctreffs@gmail.com>
2021-10-28 08:42:53 +0200

    Add timestamp

--- Sources/FirebladeTime/Time.swift
+++ Sources/FirebladeTime/Time.swift
@@ -5,6 +5,18 @@
 //  Created by Christian Treffs on 15.09.19.
 //
 
+#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+import Darwin
+#elseif os(Windows)
+import CRT
+#elseif canImport(Glibc)
+import Glibc
+#elseif canImport(WASILibc)
+import WASILibc
+#else
+#error("Unsupported runtime")
+#endif
+
 public enum Time {
     @usableFromInline static var time: TimeProviding = makeTime()
 
@@ -30,5 +42,38 @@ public enum Time {
     @inlinable
     public static func elapsed(start: Nanoseconds, end: Nanoseconds) -> Nanoseconds {
         time.elapsed(start: start, end: end)
+    }
+
+    @inlinable
+    public static func timestamp() -> String {
+        var tv = timeval()
+        gettimeofday(&tv, nil)
+        let tm = localtime(&tv.tv_sec)
+        let millis = tv.tv_usec
+        
+        if #available(macOS 11.0, iOS 14.0, tvOS 14.0, *) {
+            return String(unsafeUninitializedCapacity: 32) { cPtr in
+                cPtr.withMemoryRebound(to: CChar.self) { ptr in
+                    let pBase = ptr.baseAddress!
+                    strftime(pBase, 20, "%Y-%m-%d %H:%M:%S.", tm)
+                    let pMilli = pBase.advanced(by: 20)
+                    _ = withVaList([millis]) { vsnprintf(pMilli, 7, "%06d", $0) }
+                    let pZone = pMilli.advanced(by: 6)
+                    _ = strftime(pZone, 5, "%z", tm)
+                }
+                return 31
+            }
+        } else {
+            var buffer = [CChar](repeating: 0, count: 32)
+            return buffer.withUnsafeMutableBufferPointer { ptr -> String in
+                let pBase = ptr.baseAddress!
+                strftime(pBase, 20, "%Y-%m-%d %H:%M:%S.", tm)
+                let pMilli = pBase.advanced(by: 20)
+                _ = withVaList([millis]) { vsnprintf(pMilli, 7, "%06d", $0) }
+                let pZone = pMilli.advanced(by: 6)
+                strftime(pZone, 5, "%z", tm)
+                return String(cString: pBase)
+            }
+        }
     }
 }
